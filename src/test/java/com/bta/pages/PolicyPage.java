7c81e71d391988b70bb02e57cfaf4aeed1717a63
ubahobs <ivanovs.ivans255@gmail.com>
2024-11-23 16:46:37 +0200

    Implemented Policy Page

--- src/test/java/com/bta/pages/PolicyPage.java
+++ src/test/java/com/bta/pages/PolicyPage.java
@@ -1,4 +1,22 @@
 package com.bta.pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class PolicyPage {
+//
+    private static final SelenideElement
+        continueWithOptimalButton = $("[datatype='selectPolicyPlanOPTIMAL']");
+
+    public PolicyPage() {
+        continueWithOptimalButton.shouldBe(visible.because("Policy Page should be loaded"));
+    }
+
+    public AdditionalOptionsPage navigateToAdditionalOptionsPage() {
+        continueWithOptimalButton.click();
+
+        return new AdditionalOptionsPage();
+    }
 }
